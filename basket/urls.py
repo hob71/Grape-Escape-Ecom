3725e1d4e063635ffae25130432cc51cf880baad
hob71 <roberthobday@sky.com>
2021-01-03 17:40:24 +0000

    Added update and remove functionality to basket

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('basket/', views.basket, name='basket'),
     path('add/<item_id>/', views.item_to_basket, name='item_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def basket(request):
@@ -18,3 +18,25 @@ def item_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def update_basket(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(reverse('basket'))
+
+
+def remove_from_basket(request, item_id):
+
+    basket = request.session.get('basket', {})
+
+    basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('basket'))
