1b9e931e0268ee5bb6e28c25bb7f215bd70ea3ca
hob71 <roberthobday@sky.com>
2021-01-09 14:46:18 +0000

    Corrected sender for post_delete and .models import

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,6 @@
 # signals.py taken from Code Institue miniproject
 
-from models import orderlineitem
+from .models import orderlineitem
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
@@ -9,6 +9,7 @@ from django.dispatch import receiver
 def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
-@receiver(post_delete, sender=OrderLineItem)
+
+@receiver(post_delete, sender=orderlineitem)
 def update_on_delete(sender, instance, **kwargs):
     instance.order.update_total()
