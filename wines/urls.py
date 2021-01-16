88fca8cd53e3eab9116d1cd3d5e19207bd110558
hob71 <roberthobday@sky.com>
2021-01-16 14:19:28 +0000

    Added pages, urls and views for regions and cheeses

--- wines/urls.py
+++ wines/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/', views.add_wine, name='add_wine'),
     path('edit/<product_id>/', views.edit_wine, name='edit_wine'),
     path('delete/<product_id>/', views.delete_wine, name='delete_wine'),
+    path('add_region/', views.add_region, name='add_region'),
+    path('add_cheese/', views.add_cheese, name='add_cheese'),
 ]
--- wines/views.py
+++ wines/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import Product
-from .forms import wineform
-# From Code Institue mini project
+from .forms import wineform, regionform, cheeseform
+# From Code Institue mini project login_required
 from django.contrib.auth.decorators import login_required
 
 
@@ -61,3 +61,43 @@ def delete_wine(request, product_id):
     wine = get_object_or_404(Product, pk=product_id)
     wine.delete()
     return redirect(reverse('wines'))
+
+
+@login_required
+def add_region(request):
+
+    if request.method == 'POST':
+        form = regionform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('add_region'))
+    else:
+        form = regionform()
+
+    form = regionform()
+    template = 'wines/add_region.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def add_cheese(request):
+
+    if request.method == 'POST':
+        form = cheeseform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('add_cheese'))
+    else:
+        form = cheeseform()
+
+    form = cheeseform()
+    template = 'wines/add_cheese.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
