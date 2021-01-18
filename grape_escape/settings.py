7633e903632949be15bf5cb7653a8724c9c490a5
hob71 <roberthobday@sky.com>
2021-01-18 19:16:26 +0000

    Removed sectret key and updated debug

--- grape_escape/settings.py
+++ grape_escape/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9d=y*=7qg#1!5%$o(fsisu7afow0*(c@%tylbg0(_4$s7t!#t-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['grape-escape-ecom.heroku.com', 'localhost']
 
